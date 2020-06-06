f50cbc9c23b38085a8e6dfaba63c332ab7bf5081
blue96 <pascalfelim@yahoo.com>
2020-06-06 15:47:27 +0700

    tutorial 3

--- sesi 10 3/index.js
+++ sesi 10 3/index.js
@@ -1,30 +1,28 @@
-let tweet = []
-let kata = ''
-var txtKata = document.getElementById("txtKata");
-var batasText = document.getElementById("batasText");
- 
-
-const onSubmit = (event) => {
-    event.preventDefault();
-    if (txtKata.value.length <= 140){
-        let txtKalimat = document.getElementById('txtKata').value;
-        tweet.push(txtKalimat)
-        let kata = ''
-        tweet.forEach(element=>{
-            kata = kata + element + '<br>';
-        })
-        s = "";
-        batasText.innerText = 140-s.length + '/140';
-        document.getElementById("txtKata").value = "";
-        document.getElementById('hasil').innerHTML = kata;
-    
+let min, max, mean
+let odds = []
+let evens = []
+let total = 0
+function numberProcessing(arr){
+    max = arr[0];
+    min = arr[0];
+    for (let i = 0; i < arr.length ; i++){
+        total = total + arr[i];
+        if (arr[i] > max){
+            max = arr[i];
+        }
+        if (arr[i] < min){
+            min = arr[i];
+        }
+        if (arr[i] % 2 === 0){
+            evens.push(arr[i]);
+        }
+        else{
+            odds.push(arr[i]);
+        }
     }
-    else {
-        alert('character melebihi kapasitas')
-    }
+    mean = Math.round(total / arr.length)
+    console.log(arr)
+    console.log('min = ' + min + '; max = ' + max + '; mean = ' + mean + '; odds = ' + odds.join('-') + '; evens = ' + evens.join('-') )
 }
 
-const onChange = () => {
-    var s = txtKata.value;
-    batasText.innerText = 140-s.length + '/140';
-}
+numberProcessing([1,3,5,1,2,8,10,0,3]);
